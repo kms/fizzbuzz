845bc06ac17c90a7312220d373a01b3201eb6202
Karl-Martin Skontorp <kms@skontorp.net>
2010-01-02 23:03:44 +0100

    Add initial skeleton for FuzzBuzz generated by Maven.

---
+++ src/main/java/org/picofarad/fuzzbuzz/App.java
@@ -0,0 +1,13 @@
+package org.picofarad.fuzzbuzz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
