aa2041064ef1993f53f5616ffe819f7ad5995d91
Karl-Martin Skontorp <kms@skontorp.net>
2010-01-03 00:04:13 +0100

    Extract common code for 'fuzz' and 'buzz' into addWordIfDivisible.

--- src/main/java/org/picofarad/fuzzbuzz/FuzzBuzz.java
+++ src/main/java/org/picofarad/fuzzbuzz/FuzzBuzz.java
@@ -4,21 +4,23 @@ public class FuzzBuzz {
 	public static String play(int i) {
 		StringBuilder sb = new StringBuilder();
 
-		if (divisibleBy(i, 3)) {
-			sb.append("fuzz");
-		}
-
-		if (divisibleBy(i, 5)) {
-			if (sb.length() != 0) {
-				sb.append(" ");
-			}
-			sb.append("buzz");
-		}
+		addWordIfDivisible(i, sb, 3, "fuzz");
+		addWordIfDivisible(i, sb, 5, "buzz");
 
 		if (sb.length() == 0) {
 			return Integer.toString(i);
 		} else {
 			return sb.toString();
+		}
+	}
+
+	private static void addWordIfDivisible(int i, StringBuilder sb,
+			int divisor, String word) {
+		if (divisibleBy(i, divisor)) {
+			if (sb.length() != 0) {
+				sb.append(" ");
+			}
+			sb.append(word);
 		}
 	}
 
