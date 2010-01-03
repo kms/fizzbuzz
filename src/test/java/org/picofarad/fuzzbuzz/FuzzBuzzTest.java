4dc4bf022a308cdb00242e78de4c2e18fe73aedd
Karl-Martin Skontorp <kms@skontorp.net>
2010-01-03 01:14:58 +0100

    Combine and rename all test cases that are testing the same thing.

--- src/test/java/org/picofarad/fuzzbuzz/FuzzBuzzTest.java
+++ src/test/java/org/picofarad/fuzzbuzz/FuzzBuzzTest.java
@@ -16,57 +16,26 @@ public class FuzzBuzzTest {
 	}
 
 	@Test
-	public void oneGivesOne() throws Exception {
+	public void testPlayWithPlainNumbers() throws Exception {
 		assertEquals("1", fb.play(1));
-	}
-
-	@Test
-	public void twoGivesTwo() throws Exception {
 		assertEquals("2", fb.play(2));
 	}
 
 	@Test
-	public void threeGivesFuzz() throws Exception {
+	public void testPlayWithDivisibleByOneRule() throws Exception {
 		assertEquals("fuzz", fb.play(3));
-	}
-
-	@Test
-	public void fourGivesFour() throws Exception {
-		assertEquals("4", fb.play(4));
-	}
-
-	@Test
-	public void fiveGivesBuzz() throws Exception {
-		assertEquals("buzz", fb.play(5));
-	}
-
-	@Test
-	public void sixGivesFuzz() throws Exception {
 		assertEquals("fuzz", fb.play(6));
+
+		assertEquals("buzz", fb.play(5));
+
+		assertEquals("coconut", cfb.play(4));
 	}
 
 	@Test
-	public void tenGivesBuzz() throws Exception {
-		assertEquals("buzz", fb.play(10));
-	}
-
-	@Test
-	public void fifteenGivesFuzzBuzz() throws Exception {
+	public void testPlayWithDivisibleByMultipleRuleNumbers() throws Exception {
 		assertEquals("fuzz buzz", fb.play(15));
-	}
-
-	@Test
-	public void thirtyGivesFuzzBuzz() throws Exception {
 		assertEquals("fuzz buzz", fb.play(30));
-	}
 
-	@Test
-	public void coconutEightGivesCoconut() throws Exception {
-		assertEquals("coconut", cfb.play(8));
-	}
-
-	@Test
-	public void coconutSixtyGivesFuzzBuzzCoconut() throws Exception {
 		assertEquals("fuzz buzz coconut", cfb.play(60));
 	}
 }
